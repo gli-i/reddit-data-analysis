c40af3dd37930933280b3d419149d8b93c45ef1c
gsl5 <gsl5@sfu.ca>
2024-07-30 12:58:59 -0700

    change get_reddit_data.py to add date col and get data more efficiently

--- get_reddit_data.py
+++ get_reddit_data.py
@@ -1,6 +1,8 @@
 import sys
 import praw
 from pyspark.sql import SparkSession, types
+from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import isnan
 
 def main():
     # info for the reddit instance
@@ -20,14 +22,17 @@ def main():
         types.StructField('score', types.LongType()),
         types.StructField('upvote_ratio', types.FloatType()),
         types.StructField('num_comments', types.LongType()),
+        types.StructField('date_created', types.FloatType()),
     ])
     comms_schema = types.StructType([
         types.StructField('link_id', types.StringType()),
         types.StructField('body', types.StringType()),
         types.StructField('score', types.LongType()),
+        types.StructField('date_created', types.FloatType())
     ])
 
-    overwrite = True
+    overwrite_subs = True
+    overwrite_comms = True
 
     for subreddit_name in subreddit_names:
         subreddit = reddit.subreddit(subreddit_name)
@@ -37,24 +42,51 @@ def main():
         comments_list = []
 
         # iterate through new submissions in the subreddit
-        for submission in subreddit.new(limit=100):
-            submissions_list.append([submission.id, submission.subreddit.display_name, submission.title, submission.selftext, submission.score, submission.upvote_ratio, submission.num_comments])
+        for submission in subreddit.new(limit=500):
+            submissions_list.append(
+                [submission.id, submission.subreddit.display_name, submission.title, submission.selftext, submission.score, 
+                 submission.upvote_ratio, submission.num_comments, submission.created_utc]
+            )
 
             # iterate through comments in that submission
-            submission.comments.replace_more(limit=0)
+            submission.comments.replace_more(limit=50)
             for comment in list(submission.comments):
-                comments_list.append([comment.link_id, comment.body, comment.score])
+                comments_list.append(
+                    [comment.link_id, comment.body, comment.score, comment.created_utc]
+                )
+        print("lists created!\n")
 
         subs_df = spark.createDataFrame(submissions_list, schema=subs_schema)
         comm_df = spark.createDataFrame(comments_list, schema=comms_schema)
 
-        if overwrite: # only overwrite previous data the first time
-            subs_df.write.json("reddit-data/submissions", compression='gzip', mode='overwrite')
-            comm_df.write.json("reddit-data/comments", compression='gzip', mode='overwrite')
-            overwrite = False;
-        else:
-            subs_df.write.json("reddit-data/submissions", compression='gzip', mode='append')
-            comm_df.write.json("reddit-data/comments", compression='gzip', mode='append')
+        print("dfs created!\n")
+
+        # time must be converted from unix time to timestamp
+        subs_df = subs_df.withColumn("date_created", subs_df["date_created"].cast(TimestampType()))
+        comm_df = comm_df.withColumn("date_created", comm_df["date_created"].cast(TimestampType()))
+
+        subs_df.repartition(100)
+        comm_df.repartition(100)
+
+        while (subs_df.isEmpty == False):
+            limited_subs = subs_df.limit(100).cache()
+            subs_df = subs_df.subtract(limited_subs)
+
+            if overwrite_subs: # only overwrite previous data the first time
+                limited_subs.write.json("reddit-data/submissions", compression='gzip', mode='overwrite')
+                overwrite_subs = False
+            else:
+                limited_subs.write.json("reddit-data/submissions", compression='gzip', mode='append')
+
+        while (comm_df.isEmpty == False):
+            limited_comm = comm_df.limit(100).cache()
+            comm_df = comm_df.subtract(limited_comm)
+
+            if overwrite_comms: # only overwrite previous data the first time
+                limited_comm.write.json("reddit-data/submissions", compression='gzip', mode='overwrite')
+                overwrite_comms = False
+            else:
+                limited_comm.write.json("reddit-data/submissions", compression='gzip', mode='append')
 
 
 if __name__=='__main__':
